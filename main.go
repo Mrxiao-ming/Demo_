cbf0c5339486f3afc46ea7eaa1f8613ea05714a0
Mr小明同学 <cisco@ciscodeMacBook-Pro.local>
2021-01-13 11:25:38 +0800

    build project

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"Demo_/model/enum"
+	"Demo_/route"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var (
+		port = ":8080"
+		//env = flag.String("env", "dev", "请输入运行环境:\n  fat:测试环境\n uat:预上线环境\n product:正式环境 \n")
+	)
+
+	//flag.Parse()
+
+	fmt.Println("start time:", time.Now().Format(string(enum.FormatTime)))
+	gin.SetMode(gin.DebugMode)
+	gin.DisableConsoleColor()
+	engine := gin.New()
+	engine.Use(route.Cors())
+	route.InitRoute(engine)
+	fmt.Println("end time:", time.Now().Format(string(enum.FormatTime)))
+	_ = engine.Run(port)
 }
---
+++ model/enum/enum.go
@@ -0,0 +1,7 @@
+package enum
+
+type Time string
+
+const (
+	FormatTime Time = "2006-01-02 15:04:05"
+)
