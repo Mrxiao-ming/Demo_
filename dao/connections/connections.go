3477587e70f981f5f9325494df45349e2aed21f2
liming <liming@aiforward.com>
2021-05-25 17:42:29 +0800

    add code

--- dao/connections/connections.go
+++ dao/connections/connections.go
@@ -8,16 +8,26 @@ import (
 	"gorm.io/gorm/schema"
 )
 
+const (
+	MysqlUserName           = "root"
+	MysqlPassWordHome       = "123456"
+	MysqlPassWordCompany    = "1234"
+	MysqlHostAndPortHome    = "192.168.250.76:3306"
+	MysqlHostAndPortCompany = "localhost:3306"
+	MysqlDatabaseHome       = "xm_test"
+	MysqlDatabaseCompany    = "test"
+)
+
 type Mysql struct {
 	User string
 }
 
 func NewMysqlConnection() *gorm.DB {
 	mysqlDsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		"root",
-		"123456",
-		"192.168.250.76:3306",
-		"xm_test")
+		MysqlUserName,
+		MysqlPassWordCompany,
+		MysqlHostAndPortCompany,
+		MysqlDatabaseCompany)
 
 	db, err := gorm.Open(mysql.Open(mysqlDsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
@@ -25,7 +35,7 @@ func NewMysqlConnection() *gorm.DB {
 			SingularTable: true,  // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `t_user`
 		},
 		CreateBatchSize: 100, // 自己本地docker玩玩,就别弄太多数据了。
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger:          logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
@@ -34,3 +44,4 @@ func NewMysqlConnection() *gorm.DB {
 
 	return db
 }
+
--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module Demo_
 go 1.15
 
 require (
+	github.com/AlekSi/pointer v1.1.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/now v1.1.2 // indirect
-	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	gorm.io/driver/mysql v1.0.5
-	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.3
 )
